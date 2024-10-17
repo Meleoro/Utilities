6dd969b9eae6472537a25cd3d95107a76cf4a234
Arthur <arthur.perigno@gmail.com>
2024-10-17 07:51:24 +0200

    Lerp material color

--- Runtime/CoroutineUtilities/MaterialUtilities.cs
+++ Runtime/CoroutineUtilities/MaterialUtilities.cs
@@ -12,7 +12,7 @@ namespace Utilities
 
         private static List<Material> materialsToStop = new List<Material>();
 
-        #region Lerp Material Value
+        #region Lerp Material Float
 
         private static Dictionary<Material, Task> currentLerpedMaterials = new();
         public static void ULerpMaterialFloat(this Material material, float duration, float endValue, string parameterName)
@@ -57,6 +57,57 @@ namespace Utilities
             await Task.Yield();
 
             currentLerpedMaterials.Remove(material);
+        }
+
+        #endregion
+
+
+        #region Lerp Material Color
+
+        private static Dictionary<Material, Task> currentLerpedMaterialsColors = new();
+        public static void ULerpMaterialColor(this Material material, float duration, Color endValue, string parameterName)
+        {
+            if (currentLerpedMaterialsColors.Keys.Contains(material))
+            {
+                materialsToStop.Add(material);
+
+                currentLerpedMaterialsColors[material] = ULerpMaterialColorAsync(material, duration, endValue, parameterName);
+            }
+            else
+            {
+                currentLerpedMaterialsColors.Add(material, ULerpMaterialColorAsync(material, duration, endValue, parameterName));
+            }
+        }
+
+        private static async Task ULerpMaterialColorAsync(Material material, float duration, Color endValue, string parameterName)
+        {
+            float timer = 0;
+            Color originalValue = material.GetColor(parameterName);
+
+
+            while (timer < duration)
+            {
+                if (!Application.isPlaying) return;
+                if (materialsToStop.Contains(material) && timer != 0)
+                {
+                    materialsToStop.Remove(material);
+
+                    return;
+                }
+
+
+                timer += Time.deltaTime;
+
+                material.SetColor(parameterName, Color.Lerp(originalValue, endValue, timer / duration));
+
+                await Task.Yield();
+            }
+
+            material.SetColor(parameterName, endValue);
+
+            await Task.Yield();
+
+            currentLerpedMaterialsColors.Remove(material);
         }
 
         #endregion
